7b7abf029c1e885f81874be147edcd4c6ef473e0
Siddharth Srivastava <siddharthsrivastava76@gmail.com>
2022-01-22 21:03:40 +0000

    Permission hellhole

--- background.js
+++ background.js
@@ -1,9 +1,30 @@
 const fs = require('fs');
 require('dotenv').config();
 const {KEY} = require('./config.js')
-const { Deepgram } = require('@deepgram/sdk');
 
-const socket = new WebSocket('wss://api.deepgram.com/v1/listen', [
-    'token',
-    KEY,
-])
+navigator.mediaDevices.getUserMedia({ audio: true })
+.then(stream => {
+    console.log("CALLED")
+    const mediaRecorder = new MediaRecorder(stream, {mimeType: 'audio/webm'})
+    const socket = new WebSocket('wss://api.deepgram.com/v1/listen', ['token', 
+    '8ab30cfaef2caa33afda2dfe260d35fb86b17952'])
+
+    socket.onopen = () => {
+        mediaRecorder.addEventListener('dataavailable', event => {
+            socket.send(event.data)
+        })
+
+        mediaRecorder.start(250)
+    }
+
+    socket.onmessage = message => {
+        const data = JSON.parse(message.data)
+        const transcript = data.channel.alternatives[0].transcript
+        if (transcript && data.is_final) {
+            document.querySelector('p').textContent += ' ' + transcript
+        } 
+    }
+})
+.catch((err) => {
+    console.log(err)
+})
