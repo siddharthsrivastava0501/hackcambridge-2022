a008652afb6d29c3dd8fd7b0016072884fc4feb3
Benedek <benedekder@gmail.com>
2022-01-23 07:18:50 +0000

    fully commented

--- content-script.js
+++ content-script.js
@@ -1,8 +1,10 @@
+//Asks for access to the user's voice recording device and stores it in a MediaRecorder object
 navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
     const mediaRecorder = new MediaRecorder(stream, {mimeType: 'audio/webm'})
+    //Creates a websocket to connect to the DeepGram API
     const socket = new WebSocket('wss://api.deepgram.com/v1/listen', 
         ['token', globalThis.KEY])
-
+    //Sends real-time recorded data every 500 ms to the API 
     socket.onopen = () => {
         mediaRecorder.addEventListener('dataavailable', event => {
             socket.send(event.data)
@@ -10,29 +12,31 @@ navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
 
         mediaRecorder.start(500)
     }
-
+    //When the API sends back a message
     socket.onmessage = message => {
         const data = JSON.parse(message.data)
         const transcript = data.channel.alternatives[0].transcript
+        //Prints transcript and passes it to the processing function
         if (transcript && data.is_final) {
             document.querySelector('p').textContent += ' ' + transcript;
             processSentence(transcript);
         } 
     }
 })
-
+//3 dimensional array storing commands
 const wordArray=[["open",["tub", "tab","window", "incognito", "facebook", "github", "instagram", "netflix"]],["close",["tub", "tab","window"]],["closed",["tub", "tab","window"]],["search",["for"]],["google",[]],["add",["to","bookmarks"]]];
 
-
+//Function which processes the transcript provided by the DeepGram API
 function processSentence(sentence){
+    //Splits sentence into words
     const words = sentence.split(" ");
     index=words.indexOf("chrome");
-    console.log("index");
-    console.log(index);
+    //Checks if chrome is in the transcript so there is a possibility that a command was given by the user
     if((index==(words.length-1))||(index==(words.length-2))||(index<0)){
         return false;
     }
     found=-1;
+    //Checks for the first word of the command (directly after chrome)
     for (let i = 0; i < wordArray.length;i++){
         console.log(words[index+1]);
         if(words[index+1].localeCompare(wordArray[i][0])==0){
@@ -43,7 +47,7 @@ function processSentence(sentence){
     if(found<0){
         return false;
     }
-    console.log(found);
+    //Checks for the second (and possibly third word) of the command (starting with the second word after chrome)
     found2=-1
     for(let i=0;i<wordArray.length;i++){
         if(found==i){
@@ -65,7 +69,6 @@ function processSentence(sentence){
             }
         }
     }
-    console.log(found);
     //Start of different functions being executed depending on the input
     if(found==0){                                                           //Hears phrase 'open': restricts possible functions accordingly
         if(found2==0 || found2==1){
@@ -96,15 +99,13 @@ function processSentence(sentence){
             chrome.windows.getCurrent({}, function(win){chrome.windows.remove(win.id)});                                    //Close window
         }
     }else if(found==3){
-        str=""
+        str="" //Rebuild string after command to enable searching for it
         for (let i = index+3; i < words.length; i++) {
             str+=words[i];
             str+=" " ;
         }
-        console.log(str);
-
         chrome.tabs.create({url: 'https://www.google.com/search?q=' + str});                                                //Search phrase
-    }else if(found==4){/*Google (things from index+2)*/
+    }else if(found==4){//Rebuild string after command to enable searching for it
         str=""
         for (let i = index+2; i < words.length; i++) {
             str+=words[i];
